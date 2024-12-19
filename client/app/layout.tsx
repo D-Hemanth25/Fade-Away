4dd72724ef698a389f471ac18c52e1f6d3aa1e61
D-Hemanth25 <mailhemanth25@gmail.com>
2024-12-19 20:07:20 +0530

    added loader

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="h-screen w-screen flex items-center justify-center bg-black">
+          {children}
+        </div>
       </body>
     </html>
   );
---
+++ client/app/loading.tsx
@@ -0,0 +1,11 @@
+import { BounceLoader } from "react-spinners"
+
+const Loading = () => {
+  return (
+    <div>
+      <BounceLoader color="#AE8DDD" />
+    </div>
+  )
+}
+
+export default Loading
